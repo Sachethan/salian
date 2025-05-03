4d8ec6fb4c602a4004892ee5bd3cdaf513447429
Sachethan <96675924+Sachethan@users.noreply.github.com>
2025-05-03 17:05:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const nav = document.getElementById('navLinks');
-  nav.classList.toggle('active');
-}
-
-document.getElementById('themeToggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
